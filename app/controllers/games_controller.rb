d045500f20dae9a61e3d070d7a2cf2d1f9028825
ReeshaMartis <reeshamartis@gmail.com>
2026-02-06 16:34:13 -0500

    Added Game controller -  play module

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -21,16 +21,92 @@ class GamesController < ApplicationController
             player1: player1,
             player2: player2,
             cur_round: 0,
-            rounds: []
+            p1_wins:0 ,
+            p2_wins: 0,
+            rounds:[]
         }.to_json)
-
-        render json: { gameid: game_id, msg: "Game started" }, status: :created
+        render json: { gameid: game_id, msg: "Game started! Players get ready" }, status: :created
     end
 
     #game play
     #post /games/:id/round
     def play
+
+        game_id = params[:id]
+        stored = InMemoryStore.get(game_id)
+
+        if stored.nil? 
+            render json: {msg: "game not found"}, status: :not_found
+            return
+        end
+
+        gamedata = JSON.parse(stored,symbolize_names: true)
+
+
+        if gamedata[:cur_round] >=5 || gamedata[:p1_wins] ==3 ||gamedata[:p2_wins] ==3
+            if  gamedata[:p1_wins]>gamedata[:p2_wins]
+                game_winner = gamedata[:player1]
+            elsif gamedata[:p2_wins]>gamedata[:p1_wins]
+                game_winner = gamedata[:player2]
+            else
+                game_winner = "No winner!"
+            end
+            
+            render json: {
+                msg: "game over",
+                winner: game_winner,
+                score: {
+                    p1: gamedata[:p1_wins],
+                    p2: gamedata[:p2_wins]
+                }
+            }
+            return
+        end
+
+        p1_roll = rand(1..6)
+        p2_roll = rand(1..6)
+
+        winner = nil
+
+
+        if p1_roll > p2_roll
+            gamedata[:p1_wins]+=1
+            winner = gamedata[:player1]
+        elsif p2_roll > p1_roll
+            gamedata[:p2_wins]+=1
+            winner = gamedata[:player2]
+        else 
+             winner= "Draw" 
+        end
+
+        gamedata[:cur_round] +=1
+
+        gamedata[:rounds] << {
+                round:gamedata[:cur_round] ,
+                p1_score: p1_roll,
+                p2_score: p2_roll,
+                winner: winner
+        }
+
+        InMemoryStore.set(game_id,gamedata.to_json)
+
+
+        render json: {
+            round:gamedata[:cur_round] ,
+            p1_roll: p1_roll ,
+            p2_roll: p2_roll ,
+            winner: winner,
+            score: {
+                p1: gamedata[:p1_wins],
+                p2: gamedata[:p2_wins]
+            }
+        }
+
+      
+        
+
     end
+
 
     # view current game score through redis
     # get /games/:id
