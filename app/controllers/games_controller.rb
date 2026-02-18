a9330dbb384256b6a7ed265a786daaa2f1014f24
ReeshaMartis <reeshamartis@gmail.com>
2026-02-17 20:57:33 -0500

    auto rubocop lint fixes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,48 +1,47 @@
 class GamesController < ApplicationController
     skip_before_action :verify_authenticity_token
-    #game start
-    #POST /games
-    #req : {p1name: , p2name: }
+    # game start
+    # POST /games
+    # req : {p1name: , p2name: }
     # +ve resp : {gameid: , msg : "game started"}
-    # -ve resp : common errors : player name missing, gameid not generated 
+    # -ve resp : common errors : player name missing, gameid not generated
     def create
         player1 = game_params[:player1]
-        player2 = game_params[:player2] 
+        player2 = game_params[:player2]
 
         if player1.blank? || player2.blank?
-            render json: {error: "Player names cannot be empty"}, status: :bad_request
+            render json: { error: "Player names cannot be empty" }, status: :bad_request
             return
         end
 
-        game_id = SecureRandom.hex(4) 
+        game_id = SecureRandom.hex(4)
 
-        #using inmemorystore
+        # using inmemorystore
         InMemoryStore.set(game_id, {
             player1: player1,
             player2: player2,
             cur_round: 0,
-            p1_wins:0 ,
+            p1_wins: 0,
             p2_wins: 0,
-            rounds:[]
+            rounds: []
         }.to_json)
         render json: { gameid: game_id, msg: "Game started! Players get ready" }, status: :created
     end
 
-    #game play
-    #post /games/:id/play
+    # game play
+    # post /games/:id/play
     def play
-
         game_id = params[:id]
         stored = InMemoryStore.get(game_id)
 
-        if stored.nil? 
-            render json: {msg: "game not found"}, status: :not_found
+        if stored.nil?
+            render json: { msg: "game not found" }, status: :not_found
             return
         end
 
-        gamedata = JSON.parse(stored,symbolize_names: true)
+        gamedata = JSON.parse(stored, symbolize_names: true)
 
-        #game end condition
+        # game end condition
         if gamedata[:cur_round] >=5 || gamedata[:p1_wins] ==3 ||gamedata[:p2_wins] ==3
             if  gamedata[:p1_wins]>gamedata[:p2_wins]
                 game_winner = gamedata[:player1]
@@ -51,7 +50,7 @@ class GamesController < ApplicationController
             else
                 game_winner = "No winner!"
             end
-            
+
             render json: {
                 msg: "game over",
                 winner: game_winner,
@@ -62,7 +61,7 @@ class GamesController < ApplicationController
             }
             return
         end
-        #game start
+        # game start
         p1_roll = rand(1..6)
         p2_roll = rand(1..6)
         winner = nil
@@ -73,26 +72,26 @@ class GamesController < ApplicationController
         elsif p2_roll > p1_roll
             gamedata[:p2_wins]+=1
             winner = gamedata[:player2]
-        else 
-             winner= "Draw" 
+        else
+             winner= "Draw"
         end
 
         gamedata[:cur_round] +=1
 
         gamedata[:rounds] << {
-                round:gamedata[:cur_round] ,
+                round: gamedata[:cur_round],
                 p1_score: p1_roll,
                 p2_score: p2_roll,
                 winner: winner
         }
 
-        #results stored and rendered
-        InMemoryStore.set(game_id,gamedata.to_json)
+        # results stored and rendered
+        InMemoryStore.set(game_id, gamedata.to_json)
 
         render json: {
-            round:gamedata[:cur_round] ,
-            p1_roll: p1_roll ,
-            p2_roll: p2_roll ,
+            round: gamedata[:cur_round],
+            p1_roll: p1_roll,
+            p2_roll: p2_roll,
             winner: winner,
             score: {
                 p1: gamedata[:p1_wins],
@@ -108,15 +107,14 @@ class GamesController < ApplicationController
         game_id = params[:id]
         stored = InMemoryStore.get(game_id)
 
-        if stored.nil? 
-            render json: {msg: "game not found"}, status: :not_found
+        if stored.nil?
+            render json: { msg: "game not found" }, status: :not_found
             return
         end
 
-        gamedata = JSON.parse(stored,symbolize_names: true)
+        gamedata = JSON.parse(stored, symbolize_names: true)
 
         render json: gamedata
-
     end
 
     # view history of games played - stored in DB
@@ -127,6 +125,6 @@ class GamesController < ApplicationController
     private
 
     def game_params
-        params.require(:game).permit(:player1,:player2)
+        params.require(:game).permit(:player1, :player2)
     end
 end
